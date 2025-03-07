9e6fd98b0439ff1c9804d45215d9a3f226f10f78
unknown <neet693@gmail.com>
2025-03-07 17:51:39 +0700

    add canAccessPanel usermodel salah email

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@premiumpatungan.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@patunganpremium.com') && $this->hasVerifiedEmail();
     }
 
     /**
